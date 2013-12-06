237591779bd387423b1c227aebba75038957d423
Arthur Chang <halcyonbeing@gmail.com>
2013-12-05 20:04:26 -0500

    Adding the files

---
+++ src/entities/AttendanceState.java
@@ -0,0 +1,7 @@
+package entities;
+
+public enum AttendanceState {
+	NOT_TAKEN,
+	BEING_TAKEN,
+	TAKEN;
+}
---
+++ src/entities/QuizState.java
@@ -0,0 +1,8 @@
+package entities;
+
+public enum QuizState {
+	NOT_TAKEN,
+	BEING_TAKEN,
+	TAKEN,
+	HIDDEN;
+}
